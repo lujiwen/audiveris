577c6dde3c79fb80410c377efc978925b2bc56fc
lujiwen <1281476227@qq.com>
2018-10-21 23:20:07 +0800

    can retrive file from parameter of request function

--- src/main/kotlin/org/jetbrains/kotlin/demo/TranscribeController.kt
+++ src/main/kotlin/org/jetbrains/kotlin/demo/TranscribeController.kt
@@ -1,18 +1,27 @@
 package org.jetbrains.kotlin.demo
 
-import org.springframework.web.bind.annotation.GetMapping
+import com.sun.javafx.tools.packager.Log
+import lombok.extern.slf4j.Slf4j
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import java.util.concurrent.atomic.AtomicLong
+import org.springframework.web.multipart.MultipartFile
+import java.io.File
 
 @RestController
+@Slf4j
 class TranscribeController {
 
-    val counter = AtomicLong()
 
-    @GetMapping("/transcribe")
-    fun transcribe() {
-        val args = arrayOf("-batch", "-export", "-output", "./data/examples/output", "--", "./data/examples/carmen.png")
-        org.audiveris.omr.Main.main(args)
+    @PostMapping("/transcribe")
+    fun transcribe(@RequestParam("file") file: MultipartFile): File {
+
+        val outDir = "./data/examples/output/"
+        val outputFile = File(outDir + file.name.split(".").first() + ".mxml")
+        val args = arrayOf("-batch", "-export", "-output", outDir, "--", file.originalFilename)
+        Log.debug(file.originalFilename + "will be transcribed!" )
+//        org.audiveris.omr.Main.main(args)
+        return outputFile
     }
 
 
---
+++ src/test/TranscribeControllerTest.kt
@@ -0,0 +1,20 @@
+package org.jetbrains.kotlin.demo
+
+
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+
+
+@RunWith(SpringRunner::class)
+@AutoConfigureMockMvc
+@SpringBootTest
+class TranscribeControllerTest {
+
+//    @Autowired
+//    val mvc = MockMvc()
+
+}
