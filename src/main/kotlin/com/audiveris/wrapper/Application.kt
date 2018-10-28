96c606f36e5a50e8348ac48b6b2a1aad64a12a8f
lujiwen <1281476227@qq.com>
2018-10-28 13:16:01 +0800

    enabled 'Access-Control-Allow-Origin'

--- src/main/kotlin/com/audiveris/wrapper/Application.kt
+++ src/main/kotlin/com/audiveris/wrapper/Application.kt
@@ -2,10 +2,25 @@ package com.audiveris.wrapper
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+
+
 
 @SpringBootApplication
 class Application
 
 fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java, *args)
+}
+
+@Configuration
+@EnableWebMvc
+class WebConfig : WebMvcConfigurerAdapter() {
+
+    override fun addCorsMappings(registry: CorsRegistry?) {
+        registry!!.addMapping("/**")
+    }
 }
