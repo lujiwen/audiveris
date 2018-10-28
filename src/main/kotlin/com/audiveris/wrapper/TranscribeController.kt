67396396fd61700623ca6e18a33407703db9462a
lujiwen <1281476227@qq.com>
2018-10-28 12:16:56 +0800

    added logger and changed return type to string

--- src/main/kotlin/com/audiveris/wrapper/TranscribeController.kt
+++ src/main/kotlin/com/audiveris/wrapper/TranscribeController.kt
@@ -2,30 +2,44 @@ package com.audiveris.wrapper
 
 import com.sun.javafx.tools.packager.Log
 import lombok.extern.slf4j.Slf4j
+import org.audiveris.omr.log.LogUtil
+import org.audiveris.omr.util.FileUtil
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 import java.io.File
+import org.slf4j.LoggerFactory
 
 @RestController
 @Slf4j
 class TranscribeController {
 
     @PostMapping("/transcribe")
-    fun transcribe(@RequestParam("file") file: MultipartFile): File? {
+    fun transcribe(@RequestParam("file") file: MultipartFile): String {
         if (! file.isEmpty) {
-            val outDir = "./data/examples/output/"
+            val outDir = "data/examples/output/"
             val tempFile = File.createTempFile("temp", ".png")
             val tempFileName = tempFile.name.split(".").first()
             val outputFile = File("$outDir/$tempFileName/$tempFileName.xml")
+
             file.transferTo(tempFile)
             Log.info("temp file created : " +  tempFile.absolutePath)
             val args = arrayOf("-batch", "-export", "-output", outDir, "--", tempFile.absolutePath )
             Log.debug(file.originalFilename + "will be transcribed!" )
-            org.audiveris.omr.Main.main(args)
-            return outputFile
+            return try {
+                org.audiveris.omr.Main.main(args)
+                outputFile.absolutePath
+            } catch (e: Exception) {
+                logger.debug("transcribe file ${file.originalFilename} failed ")
+                e.printStackTrace()
+                ""
+            }
         }
-        return null
+        return ""
+    }
+
+    companion object LogHelper{
+        val logger  = LoggerFactory.getLogger("TranscribeController")
     }
 }
