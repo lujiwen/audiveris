725c0b4fa9393c8b47c27ceb0ff4527326015fb7
lujiwen <1281476227@qq.com>
2019-02-16 20:38:53 +0800

    added wrapper

---
+++ src/main/kotlin/wrapper/GreetingController.kt
@@ -0,0 +1,16 @@
+package wrapper
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
+
+@RestController
+class GreetingController {
+
+    val counter = AtomicLong()
+
+    @GetMapping("/ping")
+    fun ping() =
+            Greeting(counter.incrementAndGet(), "pong")
+
+}
---
+++ src/main/kotlin/wrapper/TranscribeController.kt
@@ -0,0 +1,51 @@
+package wrapper
+
+import lombok.extern.slf4j.Slf4j
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
+import java.io.File
+import org.slf4j.LoggerFactory
+
+@RestController
+@Slf4j
+class TranscribeController {
+
+    @PostMapping("/transcribe")
+    fun transcribe(@RequestParam("file") file: MultipartFile,
+                   @RequestParam ("userId") userId: String): String {
+        if (! file.isEmpty) {
+            val outDir = File("data/sheet-xml/$userId")
+            if(! outDir.exists()) {
+                outDir.mkdir()
+            }
+
+            val splits = file.originalFilename.split(".")
+            val postfix = splits[splits.size-1]
+            val sheetName = splits[0]
+            val tempFile = File(System.getProperty("java.io.tmpdir") + "/$userId/$sheetName.$postfix")
+            tempFile.parentFile.mkdirs()
+            tempFile.createNewFile()
+
+            val outputFile = File("$outDir/$sheetName/$sheetName.xml")
+
+            file.transferTo(tempFile)
+            val args = arrayOf("-batch", "-export", "-output", outDir.absolutePath, "--", tempFile.absolutePath )
+            return try {
+                org.audiveris.omr.Main.main(args)
+
+                outputFile.absolutePath
+            } catch (e: Exception) {
+                logger.debug("transcribe file ${file.originalFilename} failed ")
+                e.printStackTrace()
+                ""
+            }
+        }
+        return ""
+    }
+
+    companion object LogHelper{
+        val logger  = LoggerFactory.getLogger("TranscribeController")
+    }
+}
